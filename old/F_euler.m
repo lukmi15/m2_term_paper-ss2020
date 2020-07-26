7013a060d7075804db81facac1e81955bd4b2b7c
lukmi15 <lukas.mirow@web.de>
2020-07-27 01:06:26 +0200

    Deleted directory with old files...

--- old/F_euler.m
+++
@@ -1,9 +0,0 @@
-%F_euler implementation for M2 term paper
-%Author(s)			: Lukas Mirow
-%Date of creation	: 7/3/2020
-
-function [F, J] = F_euler(z, ti, h, yi, f, df)
-	F = h * f(ti, yi + z) - z;
-	J = h * df(ti + h, yi + z) - eye(size(z, 1));
-	J = sparse(J);
-end
--- old/F_trapez.m
+++
@@ -1,9 +0,0 @@
-%F_trapez implementation for M2 term paper
-%Author(s)			: Lukas Mirow
-%Date of creation	: 7/9/2020
-
-function [F, J] = F_trapez(z, ti, h, yi, f, df)
-	F = h / 2 * (f(ti, yi) + f(ti + 1, yi + z)) - z;
-	J = h / 2 * df(ti + 1, yi + z) - eye(size(z, 1));
-	J = sparse(J);
-end
